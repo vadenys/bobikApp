ce3418b2bc5b1acc84f5eecaa33ad8d82e90fcf6
VasylDenys <vadenys@gmail.com>
2024-01-25 19:48:39 -0600

    BOB-13: Cell expansion is taken out of the tableViewCell scope. Expanded cell collapses upon a tap on another row

--- bobikApp/TableVC.swift
+++ bobikApp/TableVC.swift
@@ -11,45 +11,51 @@ class TableVC: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        for i in 1...5 {
-            let man = Person(name: "Bob \(i)")
-            people.append(man)
-        }
-        
         tableView.rowHeight = UITableView.automaticDimension
         tableView.estimatedRowHeight = 100
     }
     
-    var people = [Person]()
+    var openedCellList: [Bool] = []
+    lazy var people: [Person] = {
+        var peops = [Person]()
+        for i in 1...20 {
+            let man = Person(name: "Bob \(i)")
+            peops.append(man)
+        }
+        openedCellList = Array(repeating: false, count: peops.count)
+        return peops
+    }()
     let cellID = "inboxCell"
-    
+
     // MARK: - Table view data source
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return people.count
     }
-    
+
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath) as! CustomCell
         cell.nameLabel.text = people[indexPath.row].name
-        cell.bioLabel.text = "tap to expand -->"
+        if openedCellList[indexPath.row] == false {
+            cell.bioLabel.text = "tap to expand -->"
+        } else {
+            cell.bioLabel.text = people[indexPath.row].bio
+        }
         return cell
     }
     
+    var tappeCell: UITableViewCell?
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        guard let cell = tableView.cellForRow(at: indexPath) as? CustomCell else {return}
-        cell.expandedCell.toggle()
-        let tappedMan = people[indexPath.row]
-        
-        if cell.expandedCell {
-            cell.bioLabel.text = tappedMan.bio
-            tableView.beginUpdates()
-            tableView.endUpdates()
-        } else {
-            cell.bioLabel.text = "tap to expand -->"
-            tableView.beginUpdates()
-            tableView.endUpdates()
+        if let lastCell = tappeCell as? CustomCell {
+            lastCell.bioLabel.text = "tap to expand -->"
         }
+        guard let cell = tableView.cellForRow(at: indexPath) as? CustomCell else {return}
+        openedCellList = Array(repeating: false, count: people.count)
+        openedCellList[indexPath.row] = true
+        let tappedMan = people[indexPath.row]
+        cell.bioLabel.text = tappedMan.bio
+        tableView.beginUpdates()
+        tableView.endUpdates()
+        tappeCell = cell
     }
 }
