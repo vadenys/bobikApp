c1a57555096d6d1528966a6d84e9962c3bc4cf6d
VasylDenys <vadenys@gmail.com>
2024-02-05 21:05:16 -0600

    BOB-17: textView replaces 1st label

--- bobikApp/TableVC.swift
+++ bobikApp/TableVC.swift
@@ -44,7 +44,7 @@ class TableVC: UITableViewController, UIGestureRecognizerDelegate {
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath) as! CustomCell
-        cell.nameLabel.text = people[indexPath.row].name
+        //cell.nameLabel.text = people[indexPath.row].name
         if openedCellIndex == indexPath.row {
             cell.bioLabel.text = people[indexPath.row].bio
         } else {
@@ -55,9 +55,12 @@ class TableVC: UITableViewController, UIGestureRecognizerDelegate {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if openedCellIndex == indexPath.row {
+            let tappedCell = tableView.cellForRow(at: indexPath) as! CustomCell
             return
         } else if openedCellIndex == nil {
             guard let tappedCell = tableView.cellForRow(at: indexPath) as? CustomCell else {return}
+            tappedCell.nameTextView.isUserInteractionEnabled = true
+            tappedCell.nameTextView.resignFirstResponder()
             tappedCell.bioLabel.text = people[indexPath.row].bio
             tableView.beginUpdates()
             tableView.endUpdates()
@@ -66,6 +69,7 @@ class TableVC: UITableViewController, UIGestureRecognizerDelegate {
         } else if let openedCell = openedCellIndex {
             let indexPath = IndexPath(row: openedCell, section: (tableView.numberOfSections - 1))
             if let lastTappedCell = tableView.cellForRow(at: indexPath) as? CustomCell {
+                lastTappedCell.nameTextView.isUserInteractionEnabled = false
                 lastTappedCell.bioLabel.text = "tap to expand -->"
                 tableView.beginUpdates()
                 tableView.endUpdates()
@@ -79,14 +83,17 @@ class TableVC: UITableViewController, UIGestureRecognizerDelegate {
     }
     
     @objc func closeTheCell(_ gesture: UITapGestureRecognizer) {
-        let pointView = gesture.location(in: view)
         if let openedCell = openedCellIndex {
             let indexPath = IndexPath(row: openedCell, section: tableView.numberOfSections - 1)
             let cell = tableView.cellForRow(at: indexPath) as! CustomCell
+            cell.nameTextView.isUserInteractionEnabled = false
+            cell.nameTextView.resignFirstResponder()
             cell.bioLabel.text = "tap to expand -->"
             tableView.beginUpdates()
             tableView.endUpdates()
             openedCellIndex = nil
         }
     }
+    
+    
 }
